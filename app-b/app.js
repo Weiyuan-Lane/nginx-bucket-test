2410146a4bd6cddf2950288d43519d79b1d10a03
Weiyuan Liu <weiyuan.lane@gmail.com>
2020-01-27 23:36:51 +0800

    [Feature] Added assigned configuration to rollout

--- app-b/app.js
+++ app-b/app.js
@@ -8,8 +8,25 @@ const port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(_, res) {
-  res.render('pages/index');
+app.get('/bucket/rollout-1', function(_, res) {
+  res.render('pages/index', {
+    configuration: 'Bucket',
+    rollout: '1',
+  });
+});
+
+app.get('/bucket/rollout-2', function(_, res) {
+  res.render('pages/index', {
+    configuration: 'Bucket',
+    rollout: '2',
+  });
+});
+
+app.get('/assigned', function(_, res) {
+  res.render('pages/index', {
+    configuration: 'Assigned',
+    rollout: null,
+  });
 });
 
 app.listen(port);
